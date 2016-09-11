e78b3dd209ffc5c79d858e9af0f2b2344eb17c72
Tram <tramngo2@gmail.com>
2016-09-11 11:50:16 -1000

    created function for updated display

--- js/cash_register.js
+++ js/cash_register.js
@@ -3,65 +3,70 @@ var clickOperation='null';
 var storedNumber = 0;
 var display = document.getElementById('display');
 
+function updateDisplay(content){
+   var display = document.getElementById('display');
+   display.innerHTML = display.innerHTML + content;
+}
+
 
 var nine = document.getElementById('nine');
 nine.addEventListener('click',function() {
-display.innerHTML += '9';
+updatedDisplay(9);
 });
 
 var eight = document.getElementById('eight');
 eight.addEventListener('click',function() {
-display.innerHTML += '8';
+updatedDisplay(8);
 });
 
 var seven = document.getElementById('seven');
 seven.addEventListener('click',function() {
-display.innerHTML += '7';
+updatedDisplay(7);
 });
 
 var six = document.getElementById('six');
 six.addEventListener('click', function() {
-display.innerHTML += '6';
+updatedDisplay(6);
 });
 
 var five = document.getElementById('five');
 five.addEventListener('click', function() {
-display.innerHTML += '5';
+updatedDisplay(5);
 });
 
 var four = document.getElementById('four');
 four.addEventListener('click', function() {
-display.innerHTML += '4';
+updatedDisplay(4);
 });
 
 var three = document.getElementById('three');
 three.addEventListener('click', function() {
-display.innerHTML += '3';
+updatedDisplay(3);
 });
 
 var two = document.getElementById('two');
 two.addEventListener('click', function() {
-display.innerHTML += '2';
+updatedDisplay(2);
 });
 
 var one = document.getElementById('one');
 one.addEventListener('click', function() {
-display.innerHTML += '1';
+updatedDisplay(1);
 });
 
 var zero = document.getElementById('zero');
 zero.addEventListener('click', function() {
-display.innerHTML += '0';
+updatedDisplay(0);
 });
 
 var doublez = document.getElementById('doublez');
 doublez.addEventListener('click', function() {
-display.innerHTML += '00';
+updatedDisplay(00);
 });
 
 var dot = document.getElementById('dot');
 dot.addEventListener('click', function() {
-display.innerHTML += '.';
+updatedDisplay(.);
 });
 
 var add = document.getElementById('add');
