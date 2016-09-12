70717d761763a16bada4f862317d97901edee83e
Tram <tramngo2@gmail.com>
2016-09-11 14:57:34 -1000

    correcting syntax errors

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,126 +1,131 @@
 const calc = calculator();
-var operator='null';
+var clickOperation='null';
 var storedNumber = 0;
 var display = document.getElementById('display');
 
+//create and use function to update display
+function updateDisplay(content){
+   var display = document.getElementById('display');
+   display.innerHTML = display.innerHTML + content;
+}
+
+//create and use function to clear display
+function clearDisplay() {
+   display.innerHTML = '';
+}
 
 var nine = document.getElementById('nine');
 nine.addEventListener('click',function() {
-display.innerHTML += '9';
+updateDisplay('9');
 });
 
 var eight = document.getElementById('eight');
 eight.addEventListener('click',function() {
-display.innerHTML += '8';
+updateDisplay('8');
 });
 
 var seven = document.getElementById('seven');
 seven.addEventListener('click',function() {
-display.innerHTML += '7';
+updateDisplay('7');
 });
 
 var six = document.getElementById('six');
 six.addEventListener('click', function() {
-display.innerHTML += '6';
+updateDisplay('6');
 });
 
 var five = document.getElementById('five');
 five.addEventListener('click', function() {
-display.innerHTML += '5';
+updateDisplay('5');
 });
 
 var four = document.getElementById('four');
 four.addEventListener('click', function() {
-display.innerHTML += '4';
+updateDisplay('4');
 });
 
 var three = document.getElementById('three');
 three.addEventListener('click', function() {
-display.innerHTML += '3';
+updateDisplay('3');
 });
 
 var two = document.getElementById('two');
 two.addEventListener('click', function() {
-display.innerHTML += '2';
+updateDisplay('2');
 });
 
 var one = document.getElementById('one');
 one.addEventListener('click', function() {
-display.innerHTML += '1';
+updateDisplay('1');
 });
 
 var zero = document.getElementById('zero');
 zero.addEventListener('click', function() {
-display.innerHTML += '0';
+updateDisplay('0');
 });
 
 var doublez = document.getElementById('doublez');
 doublez.addEventListener('click', function() {
-display.innerHTML += '00';
+updateDisplay('00');
 });
 
 var dot = document.getElementById('dot');
 dot.addEventListener('click', function() {
-display.innerHTML += '.';
+updateDisplay('.');
 });
 
+//load number, then clear display and then click on operation
 var add = document.getElementById('add');
 add.addEventListener('click', function() {
-	storedNumber=parseFloat(display.innerHTML='+');
+	storedNumber=parseFloat(display.innerHTML);
 	calc.load(storedNumber);
-	display.innerHTML = '';
-	operator='add';
+	clearDisplay();
+	clickOperation='add';
 });
 
 var subtract = document.getElementById('subtract');
 subtract.addEventListener('click', function() {
-	storedNumber=parseFloat(display.innerHTML='-');
+	storedNumber=parseFloat(display.innerHTML);
 	calc.load(storedNumber);
-	display.innerHTML = '';
-	operator='subtract';
+	clearDisplay();
+	clickOperation='subtract';
 });
 
 var multiply = document.getElementById('multiply');
 multiply.addEventListener('click', function() {
-	storedNumber=parseFloat(display.innerHTML='×');
+	storedNumber=parseFloat(display.innerHTML);
 	calc.load(storedNumber);
-	display.innerHTML = '';
-	operator='multiply';
+	clearDisplay();
+	clickOperation='multiply';
 });
 
 var divide = document.getElementById('divide');
 divide.addEventListener('click', function() {
-	storedNumber=parseFloat(display.innerHTML='÷');
+	storedNumber=parseFloat(display.innerHTML);
 	calc.load(storedNumber);
-	display.innerHTML = '';
-	operator='divide';
+	clearDisplay();
+	clickOperation='divide';
 });
 
 var clear = document.getElementById('clear');
 clear.addEventListener('click', function() {
-	calc.clearMemory=parseFloat(display.innerHTML='');
+	calc.clearMemory=parseFloat(display.innerHTML);
+        display.innerHTML=clearDisplay();
 });
 
-
+//use if statement to determine what happens when a certain clickOperation is chosen
 var equalSign = document.getElementById('equalSign');
 equalSign.addEventListener('click', function(){
+  if(clickOperation === 'add') {
   storedNumber = parseFloat(display.innerHTML);
-  switch (operator){
-    case 'add':
-      calc.add(storedNumber);
-      break;
-    case 'subtract':
-      calc.subtract(storedNumber);
-      break;
-    case 'multiply':
-      calc.multiply(storedNumber);
-      break;
-    case 'divide':
-      calc.divide(storedNumber);
-      break;
-  }
   display.innerHTML = calc.getTotal();
-
-});
-
-
+}if(clickOperation === 'subtract') {
+  storedNumber = parseFloat(display.innerHTML);
+  display.innerHTML = calc.getTotal();
+}if(clickOperation === 'multiply') {
+  storedNumber = parseFloat(display.innerHTML);
+  display.innerHTML = calc.getTotal();
+}if(clickOperation === 'divide') {
+  storedNumber = parseFloat(display.innerHTML);
+  display.innerHTML = calc.getTotal();
+}});
